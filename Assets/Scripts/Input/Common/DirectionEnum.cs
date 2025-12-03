c3efd5a3153612ebc3ed271badaf6db6e8efd855
Arthuro Veríssimo <arthurobdfv@gmail.com>
2025-12-03 06:34:11 +0000

    Initial Project setup with minimal Input handling

---
+++ Assets/Scripts/Input/Common/DirectionEnum.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main enumerable for game directions.
+/// </summary>
+public enum DirectionEnum
+{
+    DOWN = 6,
+    LOWER_LEFT = 7,
+    LEFT = 0,
+    UPPER_LEFT = 1,
+    UP = 2,
+    UPPER_RIGHT = 3,
+    RIGHT = 4,
+    LOWER_RIGHT = 5,
+    DIRECTION_NONE = 99
+}
